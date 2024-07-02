5c5c7d87ca4c0c14c123efe57ed09e6987569ae8
winter-jackson <winter.jackson@snarkhealth.com>
2024-07-02 13:43:29 +0300

    progress

--- src/app/api/patients/[name]/route.ts
+++ src/app/api/patients/[name]/route.ts
@@ -4,23 +4,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = require("@/lib/prisma")
 
-export async function getPatientByName(name: string) {
-    try {
-        const patient = await prisma.patient.findUnique({
-            where: { name },
-        });
-        return patient;
-    } catch (error) {
-        console.error("Failed to get patient by name:", error);
-        throw new Error("Failed to get patient by name");
-    }
-}
-
 export async function GET(req: NextRequest, { params }: { params: { name: string } }) {
     const { name } = params;
 
     try {
-        const patient = await getPatientByName(name as string);
+        const patient = await prisma.patient.findUnique({
+            where: { name },
+        });
         if (!patient) {
             return NextResponse.json({ error: "Patient not found" }, { status: 404 });
         }
