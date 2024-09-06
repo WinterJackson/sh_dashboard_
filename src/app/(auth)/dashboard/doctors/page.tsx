5926842031c9be6d84881ae49ec3d3fa9c3869d0
wj <winterjacksonwj@gmail.com>
2024-09-06 11:20:51 +0300

    Update page.tsx

--- src/app/(auth)/dashboard/doctors/page.tsx
+++ src/app/(auth)/dashboard/doctors/page.tsx
@@ -2,6 +2,8 @@
 
 import DoctorsCard from '@/components/doctors/DoctorsCard';
 import React, { useState } from 'react'
+import Image from 'next/image';
+
 
 type Props = {}
 
@@ -17,21 +19,21 @@ export default function DoctorsPage({}: Props) {
             <div className="relative flex gap-4 items-center">
                 <p className="font-semibold">Filter By</p>
                 <button onClick={() => setShowFilterOptions(!showFilterOptions)}>
-                    <img src="/images/Arrow - Right 2.svg" alt="" />
+<Image src="/images/Arrow - Right 2.svg" alt="Arrow Right" width={24} height={24} />
                 </button>
                 {showFilterOptions &&
                     <div className="absolute z-20 top-6 -right-4 flex flex-col gap-2 bg-white p-5 w-[200px] rounded-md shadow-lg" onMouseLeave={() => setShowFilterOptions(false)}>
                         <button className="flex justify-between items-center py-3 px-1 hover:bg-slate-200">
                             <p className='font-semibold'> Department</p>
-                            <img src="/images/Arrow - Right 3.svg" alt="" />
+<Image src="/images/Arrow - Right 3.svg" alt="" width={24} height={24} />
                         </button>
                         <button className="flex justify-between items-center py-3 px-1 hover:bg-slate-200">
                             <p className='font-semibold'> Availability</p>
-                            <img src="/images/Arrow - Right 3.svg" alt="" />
+<Image src="/images/Arrow - Right 3.svg" alt="" width={24} height={24} />
                         </button>
                         <button className="flex justify-between items-center py-3 px-1 hover:bg-slate-200">
                             <p className='font-semibold'> Date Joined</p>
-                            <img src="/images/Arrow - Right 3.svg" alt="" />
+<Image src="/images/Arrow - Right 3.svg" alt="" width={24} height={24} />
                         </button>
                     </div>
                 }
